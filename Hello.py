4abd9ab72684140c2adf2ebb166df83fdec16f08
dofandhuy <doanhuy205@gmail.com>
2026-01-08 15:04:13 +0700

    Uncomment and organize code in Hello.py

--- Hello.py
+++ Hello.py
@@ -1,40 +1,45 @@
-# str1 = "HELLOMyNameHuy"
-# # print(str1[:])
-# # print(str1[0:])
-# # print(str1[:5])
-# # print(str1[:3])
-# # print(len(str1))
-# print(str1.lstrip())
-# print(str1.replace('L','t'))
-# x= "n" in str1
-# print(x)
-# str2= "xin chào tôi là {}, năm tôi {} tuổi , đến từ {}"
-# print(str2.format("Huy", 22, "Hà Nội"))
-# print(max(str1))
-# print(max(str2))
-# print(min(str1))
-# print(min(str2))
-# print(str1.replace("L","n"))
-# print(str2.title())
-# import time;
-# x=time.asctime( time.localtime(time.time()))
-# print("Thời gian hiện tai: " ,x )
-
-# def printme( str ):
-#     "Chuỗi được truyền vào trong hàm"
-#     print(str)
-#     return;
-
-# printme("hello xin chào mọi người ")
+str1 = "HELLOMyNameHuy"
+print(str1[:])
+print(str1[0:])
+print(str1[:5])
+print(str1[:3])
+print(len(str1))
+print(str1.lstrip())
+print(str1.replace('L','t'))
+x= "n" in str1
+print(x)
+str2= "xin chào tôi là {}, năm tôi {} tuổi , đến từ {}"
+print(str2.format("Huy", 22, "Hà Nội"))
+print(max(str1))
+print(max(str2))
+print(min(str1))
+print(min(str2))
+print(str1.replace("L","n"))
+print(str2.title())
 
 
-# def sum(args1, args2):
-#     total= args1+args2
-#     print("Bên trong hàm: ", total)
-#     return total;
 
-# total = sum(10,20)
-# print("Bên ngoài hàm: ", total)
+
+
+import time;
+x=time.asctime( time.localtime(time.time()))
+print("Thời gian hiện tai: " ,x )
+
+def printme( str ):
+    "Chuỗi được truyền vào trong hàm"
+    print(str)
+    return;
+
+printme("hello xin chào mọi người ")
+
+
+def sum(args1, args2):
+    total= args1+args2
+    print("Bên trong hàm: ", total)
+    return total;
+
+total = sum(10,20)
+print("Bên ngoài hàm: ", total)
 
 def changeme( mylist ):
     "Thay doi list da truyen cho ham nay0"
@@ -62,4 +67,5 @@ printinfo(70,90,50)
 
 
 square =lambda x1: x1*x1 
+
 print("Bình phương của số là", square(10))
